c5f8cb8c15e41cd738cb75699dcd551a56a5fbf7
Varun jha <jhavarun999@gmail.com>
2023-04-18 00:36:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './index.css';
 
 function App() {
+  const [redditData, setRedditData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://www.reddit.com/r/reactjs.json')
+      .then(response => response.json())
+      .then(data => setRedditData(data.data.children))
+      .catch(error => console.error(error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+      {redditData.map((child) => {
+        const { title, selftext_html, url, score } = child.data;
+        return (
+          <div className='parent' key={child.data.id} style={{ width: '300px', margin: '20px', padding: '10px', backgroundColor: '#f5f5f5', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)', borderRadius: '5px' }}>
+            <div className='child' style={{ fontSize: '20px', fontWeight: 'bold', marginBottom: '10px' }}>{title}</div>
+            <div className='child' style={{ fontSize: '16px', fontWeight: 'bold', color: '#ff4500', marginBottom: '10px' }}>Score: {score}</div>
+            <div className='child' style={{ fontSize: '16px', marginBottom: '10px' }} dangerouslySetInnerHTML={{ __html: selftext_html }} />
+            <div className='child' style={{ fontSize: '16px', fontStyle: 'italic' }}>{url}</div>
+          </div>
+        );
+      })}
     </div>
   );
 }
